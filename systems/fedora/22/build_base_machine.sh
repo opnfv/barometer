215a65015908064b6ca996286584933002f0287a
Maryam Tahhan <maryam.tahhan@intel.com>
2016-05-19 14:18:49 +0100

    systems: add installation scripts
    Add installation scripts that install all dependencies for collectd with
    DPDK.
    
    Change-Id: I6f641e8714727106302185fd1fdc6c1c118a5d58
    Signed-off-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: Christoph Meyer <christoph.meyer@ericsson.com>
    Reviewed-by: Bruce Richardson <bruce.richardson@intel.com>
    Reviewed-by: Thomas Monjalon <thomas.monjalon@6wind.com>
    Reviewed-by: Jarmo Virtanen <jarmo.virtanen@nsn.com>
    Reviewed-by: John McNamara <john.mcnamara@intel.com>

---
+++ systems/fedora/22/build_base_machine.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Build a base machine for Fedora 22
+#
+# Copyright 2015 OPNFV, Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Contributors:
+#   Aihua Li, Huawei Technologies.
+#   Martin Klozik, Intel Corporation.
+#   Abdul Halim, Intel Corporation.
+
+dnf -y install $(echo "
+# Make and Compilers
+make
+automake
+gcc
+gcc-c++
+libxml2
+glibc
+glib2-devel
+kernel-devel
+kernel-modules-extra
+pixman-devel
+openssl-devel
+
+# tools
+curl
+autoconf
+libtool
+libpcap-devel
+libnet
+vim
+wget
+git
+pciutils
+cifs-utils
+socat
+
+# install git-review tool
+git-review
+" | grep -v ^#)
