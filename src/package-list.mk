13db1a28132329dcee05d9523782d1e5acffbfaf
Maryam Tahhan <maryam.tahhan@intel.com>
2016-05-19 13:07:14 +0100

    src: add makefiles to clone and build dependencies
    Add Makefiles to clone and build DPDK and collectd-with-DPDK.
    
    Change-Id: Icb0e95e4b8ad758562bfd009850b9522e7a0418a
    Signed-off-by: Maryam Tahhan <maryam.tahhan@intel.com>
    Reviewed-by: christoph.meyer@ericsson.com
    Reviewed-by: bruce.richardson@intel.com
    Reviewed-by: thomas.monjalon@6wind.com
    Reviewed-by: jarmo.virtanen@nsn.com
    Reviewed-by: john.mcnamara@intel.com

---
+++ src/package-list.mk
@@ -0,0 +1,13 @@
+# Upstream Package List
+#
+# Everything here is defined as its suggested default
+# value, it can always be overriden when invoking Make
+
+# dpdk section
+# DPDK_URL ?= git://dpdk.org/dpdk
+DPDK_URL ?= http://dpdk.org/git/dpdk
+DPDK_TAG ?= v16.04
+
+# collectd section
+COLLECTD_URL ?= https://github.com/maryamtahhan/collectd-with-DPDK
+COLLECTD_TAG ?= dpdkstat
