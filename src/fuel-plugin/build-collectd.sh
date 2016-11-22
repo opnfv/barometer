6e733e1cbc42df0b839baead51629fdcb6fb2c7c
Guo Ruijing <ruijing.guo@intel.com>
2016-11-22 12:47:17 -0500

    Enable collectd in ubuntu 16.04
    Change-Id: I67d8d36dab9cdd51276dc2c3e8b514b426005cad
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/fuel-plugin/build-collectd.sh
+++ src/fuel-plugin/build-collectd.sh
@@ -5,19 +5,18 @@ BUILD_HOME="$(pwd)"
 set -eux
 sudo apt-get -y install build-essential dh-autoreconf fakeroot  devscripts dpkg-dev git wget
 
-sudo apt-get -y install debhelper po-debconf dh-systemd \
-    bison  flex autotools-dev libltdl-dev pkg-config \
-    dh-strip-nondeterminism \
+sudo apt-get -y install \
+    debhelper dpkg-dev po-debconf dh-systemd dh-strip-nondeterminism \
+    bison flex autotools-dev libltdl-dev pkg-config \
     iptables-dev \
     javahelper \
     libatasmart-dev \
     libcap-dev \
     libcurl4-gnutls-dev \
-    libcurl3-gnutls-dev \
     libdbi0-dev \
     libesmtp-dev \
     libganglia1-dev \
-    libgcrypt20-dev \
+    libgcrypt11-dev \
     libglib2.0-dev \
     libgps-dev \
     libhiredis-dev \
@@ -27,24 +26,26 @@ sudo apt-get -y install debhelper po-debconf dh-systemd \
     liblvm2-dev \
     libmemcached-dev \
     libmodbus-dev \
-    libmosquitto-dev \
     libmnl-dev \
+    libmosquitto-dev \
     libmysqlclient-dev \
     libnotify-dev \
     libopenipmi-dev \
     liboping-dev \
     libow-dev \
-    libpcap-dev \
+    libpcap0.8-dev \
+    libpcap-dev\
     libperl-dev \
     libpq-dev \
     libprotobuf-c-dev \
-    librabbitmq-dev \
-    librdkafka-dev \
     libriemann-client-dev \
+    librdkafka-dev \
+    librabbitmq-dev \
     librrd-dev \
     libsensors4-dev \
     libsigrok-dev \
     libsnmp-dev \
+    libsnmp9-dev \
     perl \
     libtokyocabinet-dev \
     libtokyotyrant-dev \
@@ -55,20 +56,19 @@ sudo apt-get -y install debhelper po-debconf dh-systemd \
     libxen-dev \
     libxml2-dev \
     libyajl-dev \
+    linux-libc-dev \
     default-jdk \
     protobuf-c-compiler \
-    python-dev \
-    riemann-c-client \
-    patch
+    python-dev
 
 
 cd ${BUILD_HOME}
 rm -rf collectd
-git clone https://github.com/collectd/collectd; cd collectd; git checkout ed946a1;
-git clone https://github.com/collectd/pkg-debian; cd pkg-debian; git checkout 3041e46
-patch -p1 < ../../../enable_dpdk_stats_plugin.patch mv debian ..
-cd ${BUILD_HOME}/collectd
+git clone https://github.com/collectd/collectd; cd collectd; git checkout 8acc85b
+git clone https://github.com/collectd/pkg-debian; cd pkg-debian; git checkout e5972da
+mv debian ..
 
+cd ${BUILD_HOME}/collectd
 ./build.sh
 debian/rules build || true
 debian/rules build
