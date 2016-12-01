9a00ce2a39b9c62f1e3605fea3f2642e17b44d24
Guo Ruijing <ruijing.guo@intel.com>
2016-12-01 17:43:35 -0500

    Build collectd in ubuntu 16.04 docker
    Change-Id: Icd74de46b2ca61eb4e5137a2af4448f52750d7f8
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/fuel-plugin/vagrant/build_fuel_plugin.sh
+++ src/fuel-plugin/vagrant/build_fuel_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y ruby-dev rubygems-integration python-pip rpm createrepo dpkg-dev git
+sudo apt-get install -y ruby-dev rubygems-integration python-pip rpm createrepo dpkg-dev git docker.io
 sudo gem install fpm
 sudo pip install fuel-plugin-builder
 cp -r /fuel-plugin /home/vagrant
