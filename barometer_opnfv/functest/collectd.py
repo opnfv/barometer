57ddc0dc82866b2058dfe89b10307a25dc8381bd
jose.lausuch <jose.lausuch@ericsson.com>
2017-02-15 14:24:30 +0100

    Create barometer_opnfv module
    This module will be installed in functest so that
    we can do things like
    
        from barometer_opnfv.functest import collectd
    
        result = collectd.main()
    
    This way, we can avoid running bash commands in python to
    call other python scripts.
    
    Change-Id: Id3801a30648668ac2f1885cd298b920586584a18
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ barometer_opnfv/functest/collectd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+    print "Running Baromtecollectd test suite..."
+    #
+    # TODO: implementation
+    #
+    print "Test suite successfully completed."
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
