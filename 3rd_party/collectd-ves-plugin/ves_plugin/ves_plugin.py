4c6184a433aec965b92b8bc12746cef7a2473814
Aimee Ukasick <aimeeu.opensource@gmail.com>
2017-05-24 12:33:57 -0500

    Update ApiVersion to '5' to work with VES-ONAP demo
    The ONAP portion of the VES-ONAP demo has been tested with
    ApiVersion 5
    
    JIRA: VES-11
    
    Change-Id: I6ee2a6f0a09cb7b7aa2c797b320691b7ad63c19a
    Signed-off-by: Aimee Ukasick <aimeeu.opensource@gmail.com>

--- 3rd_party/collectd-ves-plugin/ves_plugin/ves_plugin.py
+++ 3rd_party/collectd-ves-plugin/ves_plugin/ves_plugin.py
@@ -251,7 +251,7 @@ class VESPlugin(object):
             'SendEventInterval' : 20.0,
             'FunctionalRole' : 'Collectd VES Agent',
             'GuestRunning' : False,
-            'ApiVersion' : 5.1              
+            'ApiVersion' : 5
         }
         self.__host_name = None
         self.__ves_timer = None
