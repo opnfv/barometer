14cafcdaa6524b1f192abadb195ca81860739f6b
Sharada Shiddibhavi <sharada.shiddibhavi@intel.com>
2017-08-17 05:52:56 +0000

    Added plugins to barometer project to test with CSV
    This patch contains new plugins - Intel RDT, OVS Stats
    and IPMI which are testing using CSV method only.
    
    Change-Id: I097fd070f8c05b508c46c2b2d75cabbd107d901f
    Signed-off-by: Sharada Shiddibhavi <sharada.shiddibhavi@intel.com>

--- baro_tests/collectd.py
+++ baro_tests/collectd.py
@@ -1,17 +1,11 @@
-"""Executing test of plugins"""
-# -*- coding: utf-8 -*-
-
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+##############################################################################
+# Copyright (c) 2017 <Company or Individual> and others.
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 
 import requests
 from keystoneclient.v3 import client
