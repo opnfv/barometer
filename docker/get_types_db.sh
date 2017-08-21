810dac49e96193b447d231d25d6522eef2e62d02
Taras Chornyi <tarasx.chornyi@intel.com>
2017-08-21 17:09:17 +0300

    docker: Added docker container for running collectd
    Added InfluxDB and Grafana containers.
    
    Change-Id: Ifa46fac03c0175267d9cbffed5354193ad182733
    Signed-off-by: Taras Chornyi <tarasx.chornyi@intel.com>

--- docker/get_types_db.sh
+++ docker/get_types_db.sh
@@ -1,4 +1,4 @@
-# Copyright 2016 OPNFV
+# Copyright 2017 OPNFV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,11 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-LoadPlugin exec
-<Plugin exec>
-     # For OVS PMD stats plugin
-     Exec "collectd_exec" "/opt/collectd/etc/collectd.conf.d/ovs_pmd_stats.sh"
-     NotificationExec "collectd_exec" "/opt/collectd/etc/collectd.conf.d/write_notification.sh"
-</Plugin>
+wget https://raw.githubusercontent.com/collectd/collectd/collectd-5.8/src/types.db -O types.db
--- docker/run_collectd.sh
+++ docker/run_collectd.sh
@@ -1,4 +1,5 @@
-# Copyright 2016 OPNFV
+#!/bin/bash
+# Copyright 2017 OPNFV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,11 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-LoadPlugin exec
-<Plugin exec>
-     # For OVS PMD stats plugin
-     Exec "collectd_exec" "/opt/collectd/etc/collectd.conf.d/ovs_pmd_stats.sh"
-     NotificationExec "collectd_exec" "/opt/collectd/etc/collectd.conf.d/write_notification.sh"
-</Plugin>
+/opt/collectd/sbin/collectd -f
