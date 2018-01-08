89ac8a00aae5261736f37becc73e179a75d83cc1
Gordon Kelly <gordon.kelly@intel.com>
2018-01-08 16:17:21 +0000

    docker: remove zookeeper from kafka docker and add ability to specify kafka broker id         and address of zookeeper host
    Change-Id: Ib6f46cf326999dcd1440dc06ab5cf6665ec578ad
    Signed-off-by: gordonkelly <gordon.kelly@intel.com>

--- docker/kafka/start_kafka.sh
+++ docker/kafka/start_kafka.sh
@@ -13,6 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-zookeeper-server start
-kafka_2.11-1.0.0/bin/kafka-server-start.sh kafka_2.11-1.0.0/config/server.properties > kafka_2.11-1.0.0/kafka.log 2>&1
+if [ -n "$broker_id" ] 
+then
+  sed -i "s/broker.id=0/broker.id=$broker_id/" \
+  kafka_2.11-1.0.0/config/server.properties
+fi
+if [ -n "$zookeeper_node" ]
+then
+  sed -i "s/localhost:2181/$zookeeper_node:2181/" \
+  kafka_2.11-1.0.0/config/server.properties
+fi
 
+kafka_2.11-1.0.0/bin/kafka-server-start.sh kafka_2.11-1.0.0/config/server.properties > kafka_2.11-1.0.0/kafka.log 2>&1
