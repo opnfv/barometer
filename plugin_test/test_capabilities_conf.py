fbb205094f006daf0610c7d8ced8e75d644b879e
Vishal Sharma <vishaliitr7@gmail.com>
2020-06-20 16:29:11 +0530

    Automate tests for capabilities plugin
    For testing plugin outputs and metrics, test_capabilities_output.py
    starts a collectd process with the correct conf file, runs the
    required tests and then stops the process.
    For config based tests, test_capabilities_conf.py passes different
    config file path to spawn a collectd process with "-t" flag and then
    check the output obtained for warnings and errors.
    
    Signed-off-by: Vishal Sharma <vishaliitr7@gmail.com>
    Change-Id: I5d9eed0b0095be1822c3d6e4894848c52dece080

---
+++ plugin_test/test_capabilities_conf.py
@@ -0,0 +1,32 @@
+import subprocess
+import time
+import urllib
+
+
+def init_collectd(conf_file_path):
+    proc = subprocess.Popen(
+        ["/opt/collectd/sbin/collectd", "-C", conf_file_path, "-T"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    out, err = proc.communicate()
+    if out:
+        return out
+    return err
+
+
+def test_normal_plugin_load():
+    output = init_collectd("capabilities_conf/capabilities.conf")
+
+    exp = """plugin "capabilities" successfully loaded"""
+
+    assert exp in output
+
+
+def test_load_plugin_warning():
+    output = init_collectd("capabilities_conf/no_loadplugin.conf")
+
+    exp = """the plugin isn't loaded"""
+
+    assert exp in output
