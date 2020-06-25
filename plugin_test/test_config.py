c74aeeef8a5ad2c029f17130db36a34feba404ec
vaniisgh <vanisingh@live.co.uk>
2020-06-25 19:13:16 +0530

    Add containers for automating test cases for collectd plugins
    Signed-off-by: vaniisgh <vanisingh@live.co.uk>
    Change-Id: I334c6644be88396832c94127ecf62cfb37ff191f

---
+++ plugin_test/test_config.py
@@ -0,0 +1,40 @@
+##############################################################################
+# Copyright (c) 2020 Intel Corporation and OPNFV.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+"Testcases for plugin configs"
+import os
+import subprocess
+import warnings
+import re
+
+"initialise config & return output"
+def init_conf(conf_file_path):
+    status, out = subprocess.getstatusoutput(
+        "/opt/collectd/sbin/collectd -C {} -T".format(conf_file_path)
+    )
+    return out
+
+
+"load plugin that is in the plugin tests folder for any test"
+def load_plugin(name):
+    output = init_conf("configs/{}.conf".format(name))
+    exp = 'plugin "{}" successfully loaded'.format(name)
+    assert exp in output
+
+
+"assess the if the config of the plugin was loaded"
+def noload_plugin(name):
+    output = init_conf("configs/{}.conf".format(name))
+    exp = "the plugin isn't loaded"
+    assert exp in output
+
+
+"tests for logparser configs"
+def test_configs_logparser():
+    load_plugin("logparser")
+    noload_plugin("noload_logparser")
