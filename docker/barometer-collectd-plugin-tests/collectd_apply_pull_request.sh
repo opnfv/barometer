9923a7a0018b9b075b9d4b4e24a37c284fc6e299
vaniisgh <vanisingh@live.co.uk>
2020-06-29 02:31:59 +0530

    - add PR applier - fix tests
    Signed-off-by: vaniisgh <vanisingh@live.co.uk>
    Change-Id: I0da22897989f27674dba58658c2b2a2bd9619370

---
+++ docker/barometer-collectd-plugin-tests/collectd_apply_pull_request.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# Copyright 2019 Intel Corporation and OPNFV. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This files contains list of pull requests to be applied on top
+# of master branch before building collectd included in docker
+# collectd-experimental container
+
+# Space/newline separated list of pull requests IDs
+# for example:
+# PULL_REQUESTS=(3027 #reimplement delay rate
+#                3028 #other PR
+#                )
+
+PULL_REQUESTS=(
+    3045 #logparser
+    #insert another PR ID here
+              )
+
+# during rebasing/merging git requires email & name to be set
+git config user.email "barometer-experimental@container"
+git config user.name "BarometerExperimental"
+
+for PR_ID in "${PULL_REQUESTS[@]}"
+do
+    echo "Applying pull request $PR_ID"
+    git pull --rebase origin pull/$PR_ID/head
+done
--- docker/barometer-collectd-plugin-tests/test_cases/logparser_tests.py
+++ docker/barometer-collectd-plugin-tests/test_cases/logparser_tests.py
@@ -1,27 +1,36 @@
 "Testcases for the logparser plugin"
-from os import name
+import os 
 import subprocess
-import time
-#import shelex
+import warnings
+import re
 
 "load plugin that is in the plugin tests folder for any test"
 def load_plugin(name=str):
-    proc = subprocess.Popen(["/opt/collectd/sbin/collectd", "-C", "$" +"/plugin_configs/"+name+".conf", "-T"],
+    proc = subprocess.Popen(["/opt/collectd/sbin/collectd", "-C", "$PWD" +"/plugin_configs/"+name+".conf", "-T"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         )
-    return proc.communicate()
+    out, err = proc.communicate()
+    if out:
+        return out
+    
+    return err
 
 "assess the if the config of the plugin was loaded"
 def test_load_plugin(name=str):
-    out, err = load_plugin(name)
-    if out:
-        return str(out), "PASSED"
-    return str(err), "FAIL"
+    output = load_plugin(name)
+    exp =str('plugin "'+name+'" successfully loaded').encode()
+    assert exp in output
+    
 
 #TODO: add the rest of the tests
 def test_pice_notification():
+    warnings.warn("warning: test warning")
+    log = open("/var/log/syslog","r+")
+    print(log.read())
     return True
 
 def test_all_logparser():
+    subprocess.Popen(["/opt/collectd/sbin/collectd", "-C", "$PWD" +"/plugin_configs/logparser.conf", "-T"], bufsize=0, stdout=subprocess.PIPE)
     test_pice_notification()
+
